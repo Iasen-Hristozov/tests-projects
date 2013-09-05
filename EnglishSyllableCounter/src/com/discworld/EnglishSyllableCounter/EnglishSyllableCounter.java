ae99b3948c74af9492306291f4ea1adda43f92ea
iasen.hristozov@gmail.com <iasen.hristozov@gmail.com@35df0971-e772-1ea1-6ada-eebf91743304>
2013-09-05 15:18:20 +0000

    Initializing.

---
+++ EnglishSyllableCounter/src/com/discworld/EnglishSyllableCounter/EnglishSyllableCounter.java
@@ -0,0 +1,15 @@
+package com.discworld.EnglishSyllableCounter;
+
+public class EnglishSyllableCounter
+{
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args)
+   {
+      // TODO Auto-generated method stub
+
+   }
+
+}
