5184cca9719e1273f3ce8d5e1311e21db4235644
iasen.hristozov@gmail.com <iasen.hristozov@gmail.com@35df0971-e772-1ea1-6ada-eebf91743304>
2014-02-05 11:32:00 +0000

    Formating

--- EnglishSyllableCounter/src/com/discworld/EnglishSyllableCounter/CEnglishSyllableCounter.java
+++ EnglishSyllableCounter/src/com/discworld/EnglishSyllableCounter/CEnglishSyllableCounter.java
@@ -78,13 +78,16 @@ public class CEnglishSyllableCounter
 
    protected static final Pattern[] SubtractSyllables = new Pattern[]
    {
-      Pattern.compile("cial"), Pattern.compile("tia"),
+      Pattern.compile("cial"), 
+      Pattern.compile("tia"),
       Pattern.compile("cius"),
       Pattern.compile("cious"),
       Pattern.compile("giu"), // belgium!
-      Pattern.compile("ion"), Pattern.compile("iou"),
-      Pattern.compile("sia$"), Pattern.compile(".ely$") // absolutely!
-                                                        // (but not ely!)
+      Pattern.compile("ion"), 
+      Pattern.compile("iou"),
+      Pattern.compile("sia$"), 
+      Pattern.compile(".ely$") // absolutely!
+                               // (but not ely!)
    };
 
    protected static final Pattern[] AddSyllables = new Pattern[]
@@ -215,6 +218,108 @@ public class CEnglishSyllableCounter
       // Return syllable count of
       // at least one.
 
+      return Math.max(result, 1);
+   }
+
+   /**
+    * Load syllable counts map from a URL.
+    * 
+    * @param mapURL
+    *           URL for map file.
+    * @param separator
+    *           Field separator.
+    * @param qualifier
+    *           Quote character.
+    * @param encoding
+    *           Character encoding for the file.
+    * 
+    * @throws FileNotFoundException
+    *            If input file does not exist.
+    * @throws IOException
+    *            If input file cannot be opened.
+    * 
+    * @return Map with values read from file.
+    */
+   
+   /**
+    * Find number of syllables in a single English word.
+    * 
+    * @param word
+    *           The word whose syllable count is desired.
+    * 
+    * @return The number of syllables in the word.
+    */
+   public int getSyllablesPositions(String word)
+   {
+      int result = 0;
+      // Null or empty word?
+      // Syllable count is zero.
+      if((word == null) || (word.length() == 0))
+      {
+         return result;
+      }
+      // If word is in the dictionary,
+      // return the syllable count from the
+      // dictionary.
+   
+      String lcWord = word.toLowerCase(Locale.US);
+   
+      // If word is not in the dictionary,
+      // use vowel group counting to get
+      // the estimated syllable count.
+   
+      // Remove embedded apostrophes and
+      // terminal e.
+   
+      lcWord = lcWord.replaceAll("'", "").replaceAll("e$", "");
+   
+      // Split word into vowel groups.
+   
+      String[] vowelGroups = lcWord.split("[^aeiouy]+");
+   
+      // Handle special cases.
+   
+      // Subtract from syllable count
+      // for these patterns.
+   
+      for(Pattern p : SubtractSyllables)
+      {
+         Matcher m = p.matcher(lcWord);
+   
+         if(m.find())
+         {
+            result--;
+         }
+      }
+      // Add to syllable count for these patterns.
+   
+      for(Pattern p : AddSyllables)
+      {
+         Matcher m = p.matcher(lcWord);
+   
+         if(m.find())
+         {
+            result++;
+         }
+      }
+   
+      if(lcWord.length() == 1)
+      {
+         result++;
+      }
+      // Count vowel groupings.
+   
+      if((vowelGroups.length > 0) && (vowelGroups[0].length() == 0))
+      {
+         result += vowelGroups.length - 1;
+      } else
+      {
+         result += vowelGroups.length;
+      }
+   
+      // Return syllable count of
+      // at least one.
+   
       return Math.max(result, 1);
    }
 }
