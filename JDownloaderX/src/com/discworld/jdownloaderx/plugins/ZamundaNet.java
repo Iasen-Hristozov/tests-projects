68a24c15db0dfa3614d814d851b63dc84093930b
Iasen-Hristozov <iasen.hristozov@gmail.com>
2017-01-26 13:14:37 +0200

    ZamundaNet plugin updated

--- JDownloaderX/src/com/discworld/jdownloaderx/plugins/ZamundaNet.java
+++ JDownloaderX/src/com/discworld/jdownloaderx/plugins/ZamundaNet.java
@@ -62,7 +62,7 @@ public class ZamundaNet extends Plugin
                                 ptnTorrent = Pattern.compile("/download_go\\.php\\?id=(\\d+)\"[\\s]*>(.+?)</a>"),
                                 ptnMagnetLink = Pattern.compile("/magnetlink/download_go\\.php\\?id=\\d+&m=x"),
                                 ptnMagnet = Pattern.compile("magnet:\\?xt=urn:btih:[\\w]*"),
-                                ptnImage = Pattern.compile("img border=(\\\")?0(\\\")? src=\"((http:\\/\\/)?img.zamunda.net\\/bitbucket\\/(.+?))\\\""),
+                                ptnImage = Pattern.compile("img border=(\")?0(\")? src=\"(((http:\\/\\/)?img(\\d)?.zamunda.net\\/bitbucket\\/([\\d]+\\/)?)([\\w\\.]+))\""),
                                 ptnImage1 = Pattern.compile("img border=(\\\")?0(\\\")? src=\\\"((http:\\/\\/)?i.imgur.com\\/(.+?))\\\""),
                                 ptnDescription = Pattern.compile("(\u041e\u043f\u0438\u0441\u0430\u043d\u0438\u0435)(.*?)((\u0421\u0443\u0431\u0442\u0438\u0442\u0440\u0438)|(\u0412\u0438\u0434\u0435\u043e)|(NFO))"),
                                 ptnSubsunacs = Pattern.compile("((http:\\/\\/)?(www\\.)?subsunacs.net(\\/){1,2}((info\\.php\\?id=\\d+)|(get\\.php\\?id=\\d+)|(subtitles\\/.+?\\/)))"),
@@ -230,7 +230,7 @@ public class ZamundaNet extends Plugin
             {
 //               sImage = oMatcher.group(5);
 //               String sImageTmp = oMatcher.group(3).substring(oMatcher.group(3).lastIndexOf("/")+1);
-               String sImageTmp = oMatcher.group(5);
+               String sImageTmp = oMatcher.group(8);
 
 //             sImage = oMatcher.group(3);
 //               if(!sImage.contains("%"))
@@ -240,7 +240,8 @@ public class ZamundaNet extends Plugin
                  if(!isValidURI(sImageTmp))
                     sImageTmp = URLEncoder.encode(sImageTmp, "UTF-8");
                  
-               sImage = "http://img.zamunda.net/bitbucket/" + sImageTmp;
+               sImage = oMatcher.group(4) + sImageTmp;
+               
             } 
             catch(UnsupportedEncodingException e)
             {
